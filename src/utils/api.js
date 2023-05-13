f816fb4368bd092be8dc653f0171a3234c0b6bce
cyborgM1002 <mkchauhan0543@gmail.com>
2023-05-13 16:48:37 +0530

    removed unwanted files

--- src/utils/api.js
+++
@@ -1,5 +0,0 @@
-const Url = (pageNum) => {
-  return;
-  ` https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=36c1795ab9b14a19bd43f39dc7bc1e9a&pages=10&${pageNum}`;
-};
-export default Url;
